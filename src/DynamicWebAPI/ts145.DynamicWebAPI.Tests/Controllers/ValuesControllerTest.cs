065b7825fd3a14a9c46f3c8b22a2e566c193448d
thinkit-software <jhlee0514@me.com>
2016-06-09 12:43:59 +0900

    dynamic web api project added

---
+++ src/DynamicWebAPI/ts145.DynamicWebAPI.Tests/Controllers/ValuesControllerTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ts145.DynamicWebAPI;
+using ts145.DynamicWebAPI.Controllers;
+
+namespace ts145.DynamicWebAPI.Tests.Controllers
+{
+    [TestClass]
+    public class ValuesControllerTest
+    {
+        [TestMethod]
+        public void Get()
+        {
+            // 정렬
+            ValuesController controller = new ValuesController();
+
+            // 동작
+            IEnumerable<string> result = controller.Get();
+
+            // 어설션
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.Count());
+            Assert.AreEqual("value1", result.ElementAt(0));
+            Assert.AreEqual("value2", result.ElementAt(1));
+        }
+
+        [TestMethod]
+        public void GetById()
+        {
+            // 정렬
+            ValuesController controller = new ValuesController();
+
+            // 동작
+            string result = controller.Get(5);
+
+            // 어설션
+            Assert.AreEqual("value", result);
+        }
+
+        [TestMethod]
+        public void Post()
+        {
+            // 정렬
+            ValuesController controller = new ValuesController();
+
+            // 동작
+            controller.Post("value");
+
+            // 어설션
+        }
+
+        [TestMethod]
+        public void Put()
+        {
+            // 정렬
+            ValuesController controller = new ValuesController();
+
+            // 동작
+            controller.Put(5, "value");
+
+            // 어설션
+        }
+
+        [TestMethod]
+        public void Delete()
+        {
+            // 정렬
+            ValuesController controller = new ValuesController();
+
+            // 동작
+            controller.Delete(5);
+
+            // 어설션
+        }
+    }
+}
---
+++ src/DynamicWebAPI/ts145.DynamicWebAPI/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace ts145.DynamicWebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
